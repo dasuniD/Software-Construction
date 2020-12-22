1194fc2337fa9371e0010083326e00319beb51a8
Dasuni Pamoda <49312983+dasuniD@users.noreply.github.com>
2020-12-22 11:17:02 +0530

    commit

---
+++ lab1/Class.java
@@ -0,0 +1,39 @@
+
+/*
+This is a programe to find student attendance
+ */
+
+/**
+ * Date : 7/10/2017 
+ * @author Chamara
+ */
+
+public class Student {
+
+    private final String name;//student name
+    private final int attendance;//student attendance
+    static int total_attendance;
+    
+    private final int MAX_ATTENDANCE = 45;//this variable can not change
+    private final int NUM_STUDENTS = 61;
+   
+    public Student(String name,int attendance){
+        this.name = name;
+        this.attendance = attendance;
+        total_attendance += this.attendance;
+    }
+ 
+    public String showName(){//get name 
+        return this.name;
+    }    
+    
+    public double getAttendancePrecentage(){
+        //return this.attendance;
+        return (this.attendance)*100/MAX_ATTENDANCE;
+    }
+    
+    public double getAverageAttendancePrecentage(){
+        return total_attendance*100/(NUM_STUDENTS*MAX_ATTENDANCE);
+    }
+    
+}
---
+++ lab1/Main.java
@@ -0,0 +1,56 @@
+
+/**
+ * Date : 7/13/2017
+ * @author Chamara
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+public class Main {
+    
+    public static void main(String [] args){
+        
+        String line = null;
+        Student [] student = new Student[62];//declare 61 student objects
+        Random new_random = new Random();
+        int attendance;
+        
+        try{
+            FileReader file = new FileReader("co225-classlist.txt");
+            try (BufferedReader reader = new BufferedReader(file)) {
+                for(int i=0; i < student.length ; i++){
+                    attendance = new_random.nextInt(45);//genarate random int 0<x<45
+                    student[i] = new Student(reader.readLine(),attendance);
+                }
+            }
+            
+        } catch(IOException e){
+            System.out.println("Bad things happen, what do you do!" + e);
+        }
+        
+       System.out.println("names of students who's attendance " +
+"percentage is less than 80"); 
+       int j=1; 
+       
+       for(int i=0; i < student.length-1; i++){               
+                if(student[i].getAttendancePrecentage() < 80){
+                    System.out.print(j);
+ 
+                    if(student[i].showName().length()>12)
+                    System.out.print(':' + student[i].showName() + '\t');
+                    else
+                    System.out.print(':' + student[i].showName() + '\t' + '\t');
+ 
+                    System.out.println(student[i].getAttendancePrecentage());
+                    j++;
+                }
+        }  
+        System.out.printf("\nAverage Attendance Precentage: ");
+        System.out.println(student[0].getAverageAttendancePrecentage());
+
+    }    
+
+}
